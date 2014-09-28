75b4debc156b32624727599328c20b5e50ca8046
mamachanko <max.brauer@deliveryhero.com>
2014-09-28 20:06:40 +0200

    fancify with bottle

--- fancytext-api/fancytext-api.py
+++ fancytext-api/fancytext-api.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+
+from bottle import route, run, request
+from fancytext import fancy
+
+
+@route('/fancy', method='POST')
+def fancify():
+    plain_text = request.POST['text']
+    return fancy(plain_text)
+
+run(host='localhost', port=8080)
